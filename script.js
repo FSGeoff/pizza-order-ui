50a59d1ffaddf2c91b6c14cee9113166cbd7197e
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-09-25 16:55:16 -0400

    refactored to simplify code

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 // console.log("Hello world");
 var welcomeContainer = document.getElementById("welcome");
 var startButton = document.getElementById("create-pizza");
-var crustOptions = document.getElementById("crust-options");
-var crustSelection = document.getElementById("crust-selection");
+var options = document.getElementById("options");
+var selection = document.getElementById("selection");
 
 startButton.addEventListener("click", function () {
   welcomeContainer.style.display = "none";
-  renderCrustOptions();
+  renderOptions(availableCrusts);
 });
 
-function renderCrustOptions() {
-  var availableCrusts = ["Thin", "Hand Tossed", "Pan", "Deep Dish"];
+var availableCrusts = ["Thin", "Hand Tossed", "Pan", "Deep Dish"];
+var availableMeats = ["Pepperoni", "Ham", "Sausage", "Beef", "Chicken"];
 
-  for (var i = 0; i < availableCrusts.length; i++) {
+
+function renderOptions(array) {
+  
+
+  for (var i = 0; i < array.length; i++) {
     // 1. Create an element.
     var button = document.createElement("button");
     // 2. Add content
     button.setAttribute("class", "btn btn-info");
-    button.textContent = availableCrusts[i];
-    button.setAttribute("data-value", availableCrusts[i]);
+    button.textContent = array[i];
+    button.setAttribute("data-value", array[i]);
     // 3. Append to an existing element
-    crustOptions.append(button);
+    options.append(button);
   }
 }
 
-crustOptions.addEventListener("click", function (event) {
+options.addEventListener("click", function (event) {
   if (event.target.matches("button")) {
     console.log("You clicked a button");
     var selectedCrust = event.target.getAttribute("data-value");
     console.log(selectedCrust);
-    
-    crustSelection.textContent = "";
+
+    selection.textContent = "";
     var crustToDisplay = document.createElement("h3");
     crustToDisplay.textContent = selectedCrust;
-    crustSelection.append(crustToDisplay);
-
+    selection.append(crustToDisplay);
+    // TODO: Save to Local Storage
+    setTimeout(function(){
+        options.textContent = "";
+        renderOptions(availableMeats);
+    }, 1000)
   }
 });
